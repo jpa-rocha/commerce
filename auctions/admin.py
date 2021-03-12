8b1265c29b35fe343752d506af57b335e61c5e76
CptCaveira <janos.rocha@gmail.com>
2021-03-12 12:38:11 +0100

    first model creation

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import User, Listing, Bids, Comments, Category, Watchlist
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Bids)
+admin.site.register(Comments)
+admin.site.register(Category)
+admin.site.register(Watchlist)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,42 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "categories"
+    category = models.CharField(max_length=120)
+
+class Listing(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(max_length=500)
+    price = models.FloatField()
+    picture = models.ImageField()
+    category = ManyToManyField(Category, related_name="listings")
+    date_added = models.DateField(auto_now_add=True)
+    date_end = models.DateField()
+    user_id = ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+
+class Bids(models.Model):
+    class Meta:
+        verbose_name_plural = "bids"
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    item = ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    amount = models.FloatField()
+
+class Comments(models.Model):
+    class Meta:
+        verbose_name_plural = "comments"
+    user = ForeignKey(User, on_delete=models.PROTECT, related_name="comments")
+    item = ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField(max_length=750)
+
+class Watchlist(models.Model):
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name="watching")
+    item = ForeignKey(Listing, on_delete=models.CASCADE, related_name="watched")
+    
+
